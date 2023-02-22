c37fe976215ff5fee67abbc1cd81ef02a5564408
admin1 <varnitshar29@gmail.com>
2023-02-22 18:08:02 +0530

    Initial Commit

---
+++ UseObserver/Observer/Product/Data.php
@@ -0,0 +1,22 @@
+<?php
+ 
+namespace UseEvent\UseObserver\Observer\Product;
+ 
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+ 
+class Data implements ObserverInterface
+{
+    /**
+     * Below is the method that will fire whenever the event runs!
+     *
+     * @param Observer $observer
+     */
+    public function execute(Observer $observer)
+    {
+        $product = $observer->getProduct();
+        $originalName = $product->getName();
+        $modifiedName = $originalName . ' - Modified by Magento 2 Events and Observers';
+        $product->setName($modifiedName);
+    }
+}
